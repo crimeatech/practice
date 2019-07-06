cf022117b601e477b5dccd3b661784fe2fdfe33f
Yahub <johndoe@example.com>
2019-07-06 19:05:43 +0300

    Finished SearchBar component

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 import "./DropDown.scss"
+import { directive } from '@babel/types';
 
-export class DropDown extends Component {
+interface DropDownState {
+    items: string[],
+    selected: number,
+    isOpened: boolean
+}
+
+export class DropDown extends Component<{},DropDownState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            items: ["Детский сад 1", "Детский сад 2", "Детский сад 3"],
+            selected : 0,
+            isOpened : true
+        };
+    }
+
+    toggle = () => this.setState({isOpened: !this.state.isOpened});
+
+    select = (e: React.MouseEvent<HTMLDivElement, MouseEvent>, item: string) => this.setState({selected: this.state.items.indexOf(item), isOpened: false});
+
     render() {
         return (
-            <div className="DropDown">
-                
+            <div className={"DropDown"+ (this.state.isOpened ? "_opened" : '')}>
+                <div className="DropDown-Selected">{this.state.items[this.state.selected]}</div>
+                <div className={"DropDown-Arrow" + (this.state.isOpened ? "_opened" : '')} onClick={this.toggle}></div>       
+                {
+                    this.state.isOpened && 
+                    <div className="DropDown-List">
+                        {this.state.items.map((item) => <div className="DropDown-ListItem" onClick={e => this.select(e, item)}>{item}</div>)}
+                    </div>
+                }         
             </div>
         )
     }
