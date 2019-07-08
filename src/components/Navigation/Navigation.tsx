cefa4c81f20537167b4fd9ca25bfa69bc1fcdd0f
Laggay-Arthur <39773470+Laggay-Arthur@users.noreply.github.com>
2019-07-08 13:18:05 +0300

    Изменил форматирование .scss файлов

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import "./Navigation.scss"
 import RegionalSites from "../RegionalSites/RegionalSites";
 import UserCabinet from "../UserCabinet/UserCabinet";
 import ForSleeping from "../ForSleeping/ForSleeping";
+import "./Navigation.scss"
 class Navigation extends React.Component{
     public render(){
         return (
