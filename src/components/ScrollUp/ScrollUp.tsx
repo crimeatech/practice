41460a64ba45b85efad280fa1a594400d218e5c7
Yahub <johndoe@example.com>
2019-07-09 12:56:07 +0300

    Added ScrollUp

--- src/components/ScrollUp/ScrollUp.tsx
+++ src/components/ScrollUp/ScrollUp.tsx
@@ -2,13 +2,28 @@ import React, { Component } from 'react'
 import { cn } from '@bem-react/classname';
 import "./ScrollUp.scss"
 
-export class ScrollUp extends Component {
+interface ScrollUpState {
+    isShowed: boolean
+}
+
+export class ScrollUp extends Component<{}, ScrollUpState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            isShowed : false
+        };
+    }
+
+    componentDidMount(){
+        window.addEventListener('scroll', () => this.setState({isShowed: window.pageYOffset >= window.innerHeight * 0.2}));
+    }
+
     scroll = () => window.scrollTo({top: 0, behavior: "smooth"});
 
     render() {
         const cnScrollUp = cn("ScrollUp")
         return (
-            <div className={cnScrollUp()} onClick={this.scroll}></div>
+            <div className={cnScrollUp({visible: this.state.isShowed})} onClick={this.scroll}></div>
         )
     }
 }
