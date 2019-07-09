94e3703fcbd1cf93640f8cde8c6019a27ed5ec54
Yahub <johndoe@example.com>
2019-07-09 14:25:17 +0300

    Fixed DropDown, Button, Background

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from "react"
-import Navigation from "../Navigation/Navigation"
 import { cn } from '@bem-react/classname';
 import "./Wrapper.scss"
 
@@ -8,22 +7,17 @@ class Wrapper extends React.Component{
     public render(){
         const cnWrapper=cn("Wrapper");
         const cnBackground=cn("Background");
-        const cnNavigation=cn("Navigation");
         return (
             <Fragment>
                 <div className={cnBackground()}>
                     <div className={cnBackground("Top", {gradient: true})}></div>
-                    <div className={cnBackground("Bottom", {color: "white"})}></div>
                 </div>
                 <div className={cnWrapper()}>
                     {this.props.children}
-                
-                   
                 </div>
             </Fragment>
-
             );
-        }
+    }
 }
 
 export default Wrapper;
