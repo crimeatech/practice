d01d11a98f2211525a6f5930d8fcdddd0200dcfb
justned <nedim51@mail.ru>
2019-07-11 02:18:35 +0300

    i did issue #5 (block #5)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import { cn } from '@bem-react/classname';
 import "./Card.scss"
-import img from "../../assets/simferopol_gerbB.gif"
 
 interface CardProps {
     city: string,
-    places: number;
+    places: number,
+    banner: any
 }
 
 class Card extends React.Component<CardProps, {}>{
@@ -13,7 +13,7 @@ class Card extends React.Component<CardProps, {}>{
         const cnCard = cn("Card");
         return(
             <div className={cnCard()}>
-                <img className={cnCard("Image")} src={img} alt="city"/>
+                <img className={cnCard("Image")} src={this.props.banner} alt="city"/>
                 <div className={cnCard("Info")}>
                     <div className={cnCard("Title")}>{this.props.city}</div>
                     <div className={cnCard("Places")}><div className={cnCard("Count")}>{this.props.places}</div>свободных мест</div>
