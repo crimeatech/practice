530cf8334bccaeb97065d3e3a8ac2c8b1dfc6424
Yahub <johndoe@example.com>
2019-07-10 19:12:13 +0300

    Finished Modal

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { cn } from '@bem-react/classname';
 import "./FeedbackForm.scss"
 
@@ -6,43 +6,63 @@ interface FormProps {
     close : () => void
 }
 
-export class FeedbackForm extends Component<FormProps, {}> { 
+interface FormState {
+    valid: boolean
+}
+
+export class FeedbackModal extends Component<FormProps, FormState> { 
+    constructor(props: FormProps) {
+        super(props);
+        this.state = {
+            valid: true
+        };
+    }
+
+    submit = (e:any) => {e.preventDefault();this.setState({valid: false});}
+
     render() {
-        const cnFeedbackContainer = cn("FeedbackContainer");
-        return (
-            <div className={cnFeedbackContainer()}>
-                <div className={cnFeedbackContainer("Header")}>
-                    <div className={cnFeedbackContainer("Title")}>Обратная связь</div>
-                    <div className={cnFeedbackContainer("Close")} onClick={this.props.close}></div>
-                </div>
-                <form className={cnFeedbackContainer("Form")}>
-                    <div className={cnFeedbackContainer("Name")}>
-                        <label htmlFor="name">Имя *</label>  
-                        <input type="text" id="name" required/> 
+        const cnFeedbackModal = cn("FeedbackModal");
+        return ( 
+            <div className={cnFeedbackModal()}>
+                <div className={cnFeedbackModal("Overlay")}></div>
+                <div className={cnFeedbackModal("Window")}>
+                    <div className={cnFeedbackModal("Header")}>
+                        <div className={cnFeedbackModal("Title")}>Обратная связь</div>
+                        <div className={cnFeedbackModal("Close")} onClick={this.props.close}></div>
                     </div>
-                    <div className={cnFeedbackContainer("Email")}>
-                        <label htmlFor="email">Электронная почта *</label>     
-                        <input type="email" id="email" required/> 
-                    </div>
-                    <div className={cnFeedbackContainer("Select")}>
-                        <label htmlFor="select">Муниципальное образование *</label> 
+                    <form className={cnFeedbackModal("Form")}>
+                        <div className={cnFeedbackModal("Name")}>
+                            <label htmlFor="name">Имя *</label>  
+                            <input type="text" id="name" required/> 
+                            <div className={cnFeedbackModal("Error", {notvalid: !this.state.valid})}>Поле является обязательным для заполнения</div>
+                        </div>
+                        <div className={cnFeedbackModal("Email")}>
+                            <label htmlFor="email">Электронная почта *</label>     
+                            <input type="email" id="email" required/> 
+                            <div className={cnFeedbackModal("Error", {notvalid: !this.state.valid})}>Поле является обязательным для заполнения</div>
+                        </div>
+                        <div className={cnFeedbackModal("Select")}>
+                            <label htmlFor="select">Муниципальное образование *</label> 
                             <select id="select" required>
-                                <option value="" disabled selected hidden>Выбрать муниципалитет</option>
+                                <option value="" disabled hidden>Выбрать муниципалитет</option>
                                 <option value="simf">Симферополь</option>
                                 <option value="yalta">Ялта</option>
                                 <option value="kerch">Керчь</option>
                                 <option value="sev">Севастополь</option>
                             </select>
-                    </div>    
-                    <div className={cnFeedbackContainer("Textarea")}>
-                        <label htmlFor="textarea">Текст сообщения *</label>     
-                        <input type="textarea" id="textarea"/> 
-                    </div>
-                    <button type="submit" className={cnFeedbackContainer("Button")}>Отправить</button>
-                </form>
+                            <div className={cnFeedbackModal("Error", {notvalid: !this.state.valid})}>Поле является обязательным для заполнения</div>
+                        </div>    
+                        <div className={cnFeedbackModal("Textarea")}>
+                            <label htmlFor="textarea">Текст сообщения *</label>     
+                            <input type="textarea" id="textarea" required/> 
+                            <div className={cnFeedbackModal("Error", {notvalid: !this.state.valid})}>Поле является обязательным для заполнения</div>
+                        </div>
+                        <button type="submit" className={cnFeedbackModal("Button")} onClick={this.submit}>Отправить</button>
+                    </form>
+                </div> 
             </div>
         )
     }
 }
 
-export default FeedbackForm;
+export default FeedbackModal;
