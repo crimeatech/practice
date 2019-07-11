bc0a5adb9f67822f2d3fd13ce4fc78e9afdadd41
Laggay-Arthur <39773470+Laggay-Arthur@users.noreply.github.com>
2019-07-11 12:51:18 +0300

    Внес последние изменения

--- src/components/NavBlock/NavBlock-Links/NavBlock-Links.tsx
+++ src/components/NavBlock/NavBlock-Links/NavBlock-Links.tsx
@@ -28,7 +28,7 @@ class NavBlockLinks extends React.Component<{}, NavState>{
         return(
             <div className={cnNavBlockLinks()}>
                 <a href={"href"}>Публикации</a>
-                <a href="#" onClick={this.showModal}>Обратная связь</a>
+                <p onClick={this.showModal}>Обратная связь</p>
                 {this.state.modal && <FeedbackForm close={this.closeModal}/>}
             </div>
         );
